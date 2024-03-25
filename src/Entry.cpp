1d67487f8b58d69b4bed6911f0b6a2cdbf110e5c
Tsubasa6848 <komatsu_moho@outlook.com>
2024-03-25 21:02:06 +0800

    adapt: adapt GMLIB v0.9.8

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -1,28 +1,21 @@
 #include "Entry.h"
 #include "Global.h"
-#include <fmt/format.h>
-#include <functional>
-#include <ll/api/Config.h>
-#include <ll/api/io/FileUtils.h>
-#include <ll/api/plugin/NativePlugin.h>
-#include <ll/api/plugin/PluginManagerRegistry.h>
-#include <memory>
-#include <stdexcept>
+
 ll::Logger logger(PLUGIN_NAME);
 
 namespace FloatingText {
 
-namespace {
+std::unique_ptr<Entry>& Entry::getInstance() {
+    static std::unique_ptr<Entry> instance;
+    return instance;
+}
 
-std::unique_ptr<std::reference_wrapper<ll::plugin::NativePlugin>>
-    selfPluginInstance; // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
-
-auto disable(ll::plugin::NativePlugin& /*self*/) -> bool {
-    removeAllFloatingTexts();
+bool Entry::load() {
+    initConfig();
     return true;
 }
 
-auto enable(ll::plugin::NativePlugin& /*self*/) -> bool {
+bool Entry::enable() {
     initFloatingTexts();
     RegisterCommand();
     logger.info(tr("info.loaded"));
@@ -31,37 +24,16 @@ auto enable(ll::plugin::NativePlugin& /*self*/) -> bool {
     return true;
 }
 
-auto load(ll::plugin::NativePlugin& self) -> bool {
-    selfPluginInstance = std::make_unique<std::reference_wrapper<ll::plugin::NativePlugin>>(self);
-    initConfig();
+bool Entry::disable() {
+    removeAllFloatingTexts();
     return true;
 }
 
-auto unload(ll::plugin::NativePlugin& self) -> bool {
-    selfPluginInstance.reset();
+bool Entry::unload() {
+    getInstance().reset();
     return true;
-}
-
-} // namespace
-
-auto getSelfPluginInstance() -> ll::plugin::NativePlugin& {
-    if (!selfPluginInstance) {
-        throw std::runtime_error("selfPluginInstance is null");
-    }
-    return *selfPluginInstance;
 }
 
 } // namespace FloatingText
 
-extern "C" {
-_declspec(dllexport) auto ll_plugin_disable(ll::plugin::NativePlugin& self) -> bool {
-    return FloatingText::disable(self);
-}
-_declspec(dllexport) auto ll_plugin_enable(ll::plugin::NativePlugin& self) -> bool {
-    return FloatingText::enable(self);
-}
-_declspec(dllexport) auto ll_plugin_load(ll::plugin::NativePlugin& self) -> bool { return FloatingText::load(self); }
-_declspec(dllexport) auto ll_plugin_unload(ll::plugin::NativePlugin& self) -> bool {
-    return FloatingText::unload(self);
-}
-}
+LL_REGISTER_PLUGIN(FloatingText::Entry, FloatingText::Entry::getInstance());
